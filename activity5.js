83d82739a6690d3d27f88e14171f532a4e70f790
5tefanyX <stefanyazur79@gmail.com>
2025-04-05 04:44:20 -0700

    Add files via upload

---
+++ activity5.js
@@ -0,0 +1,11 @@
+let favActorFirstName = "Tom";
+let favActorLastName = "Cruz";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = "My favorite actor is " + uppercase + ". His best show is Silicon Valley.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);

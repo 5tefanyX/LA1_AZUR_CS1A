4caaf703f1f1dabf6c90493ae82dbb9fdae16eb6
5tefanyX <stefanyazur79@gmail.com>
2025-04-05 04:46:44 -0700

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,9 @@
+let name = "Stefany";
+let whatDoYouWannaBecomeInYourLife = "Data Analyst";
+let gender = "Female";
+let twitterHandle = "@sen";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
